0ae990f529c305777d4cd250b7107c0356b044ef
Nikita-mnd <nikita.morozov.mnd@gmail.com>
2025-03-02 19:15:36 +0300

    task02

--- task01/logic.cpp
+++ task01/logic.cpp
@@ -3,10 +3,12 @@
 int sum_of_numbers_on_diagonal(int m[SIZE][SIZE]) {
 	int sum = 0;
 
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 1; i < SIZE; i++) {
 
-		for (int j = i+1; j < SIZE; j++) {
-			sum += m[i][j];
+		for (int j = i-1; j < SIZE; j++) {
+			if ((j == 0 && ((i == 1) || (i == 2)))||((j==2)&&(i != 3))) {
+				sum += m[i][j];
+			}
 		}
 
 	}
